77da40d4466541d999eba0fc3f6de94f4cafe9ce
Freddie Chen <freddiechenchen@gmail.com>
2026-07-25 08:07:02 +1000

    chore(release): bump to 0.0.2
    - Prepare version code 2 for the internal Google Play release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.gooludou.shadowplanner"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "com.gooludou.shadowplanner.HiltTestRunner"
     }
