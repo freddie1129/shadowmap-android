66341cb1d9fe1f405f65c0cc4c2f8015a9ed4ebc
Freddie Chen <freddiechenchen@gmail.com>
2026-07-29 16:34:07 +1000

    chore(release): prepare 0.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.gooludou.shadowplanner"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "com.gooludou.shadowplanner.HiltTestRunner"
     }
