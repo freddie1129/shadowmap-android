31c3925a0cc968110ecf5648b35ed7fc837e524b
Freddie Chen <freddiechenchen@gmail.com>
2026-07-30 21:06:58 +1000

    chore(release): bump version to 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.gooludou.shadowplanner"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "com.gooludou.shadowplanner.HiltTestRunner"
     }
