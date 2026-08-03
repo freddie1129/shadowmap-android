7f11fe0c3182ed57fbf9b743d42439587c8344d3
Freddie Chen <freddiechenchen@gmail.com>
2026-08-03 20:29:17 +1000

    chore(release): bump version to 0.0.10
    - Prepare the patch release for Google Play deployment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.gooludou.shadowplanner"
         minSdk = 24
         targetSdk = 37
-        versionCode = 9
-        versionName = "0.0.9"
+        versionCode = 10
+        versionName = "0.0.10"
 
         testInstrumentationRunner = "com.gooludou.shadowplanner.HiltTestRunner"
     }
